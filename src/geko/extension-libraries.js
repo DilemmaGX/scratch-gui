0e306851e971306b1d64ee259fa8eaa83768e87b
官方小傲娇 <111679857+DilemmaGX@users.noreply.github.com>
2024-05-09 15:40:09 +0800

    Update extension-libraries.js

--- src/geko/extension-libraries.js
+++ src/geko/extension-libraries.js
@@ -11,6 +11,12 @@ const libs = [
         info: 'https://dilemmagx.github.io/Geko-Extension-Gallery/info.json',
         base: 'https://dilemmagx.github.io/Geko-Extension-Gallery/',
         tag: 'geko'
+    },
+    {
+        name: 'Geko',
+        info: 'https://dilemmagx.github.io/Geko-Extension-Gallery/libs/NOname.json',
+        base: 'https://extensions.turbowarp.cn/',
+        tag: 'geko'
     }
 ];
 
